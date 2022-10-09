183712d8b367c12e96c8041c01cd1487d5a34fb9
Daven Hill <djbrill03@gmaill.com>
2022-10-08 23:31:25 -0400

    Initial Commit

---
+++ saver/saverApp.swift
@@ -0,0 +1,17 @@
+//
+//  saverApp.swift
+//  saver
+//
+//  Created by Daven Hill on 10/8/22.
+//
+
+import SwiftUI
+
+@main
+struct saverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
